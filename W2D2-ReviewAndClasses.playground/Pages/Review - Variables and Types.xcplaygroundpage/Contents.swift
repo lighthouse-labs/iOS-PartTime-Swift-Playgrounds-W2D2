2fb6572a33a181633af924dd39ac52bee905e010
Sam Meech-Ward <sam@meech-ward.com>
2017-03-01 14:02:58 -0800

    Complete Variables and Types

--- W2D2-ReviewAndClasses.playground/Pages/Review - Variables and Types.xcplaygroundpage/Contents.swift
+++ W2D2-ReviewAndClasses.playground/Pages/Review - Variables and Types.xcplaygroundpage/Contents.swift
@@ -26,7 +26,7 @@ mph + 0.1
 //:
 //: Change the value in `mph` to be equal to 90, use the assignment operator `=`.
 
-
+mph = 90
 
 
 //: ### Challenge 2
@@ -34,8 +34,8 @@ mph + 0.1
 //: The `greeting` variable contains a greeting. Change the value in `greeting` to include your name. i.e. if greeting contained "Hello" and your name is "Danny", then it should now contain "Hello, Danny".
 
 greeting = "Hello"
-
-
+let person = "Sam"
+let personGreeting = "\(greeting) \(person)"
 
 
 //: Note: Make sure to do this in such a way that changing the value of `greeting` to "Aloha" changes the value here to be "Aloha, Danny"
@@ -49,10 +49,10 @@ greeting = "Hello"
 //: What type are `bicycleCount`, and `isGood`?
 
 var bicycleCount = 10
-// bicycleCount type is:
+// bicycleCount type is: Int
 
 var isGood = true
-// isGood's type is:
+// isGood's type is: Bool
 
 
 //: Swift infers the type of a variable based on the initial value. This is mostly useful, but sometimes we want to change the inferred type.
@@ -64,9 +64,9 @@ var height: Double = 10
 //: Since all our variables have types, even if we haven't specifically set them, we can't store any old value in them... only values of the right type.
 //: Test this out. Try setting the value of `count` to be `height`:
 
+//count = height
 
-
-//: It didn't work, right? They are both numbers, but `count` is an Int, and `height` is a `Double`. There are ways to convert between types, but we won't go into that just now.
+//: It didn't work, right? They are both numbers, but `count` is an Int, and `height` is a `Double`. There are ways to convert between types, but we won't go into that just now. You can delete that broken line of code now.
 
 
 
