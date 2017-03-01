592d2f0ebc9e7bbcd571e9e5f4fade53602c4d4e
Sam Meech-Ward <sam@meech-ward.com>
2017-03-01 14:14:24 -0800

    Complete Control Flow

--- W2D2-ReviewAndClasses.playground/Pages/Review - Control Flow.xcplaygroundpage/Contents.swift
+++ W2D2-ReviewAndClasses.playground/Pages/Review - Control Flow.xcplaygroundpage/Contents.swift
@@ -39,7 +39,11 @@ if someThing > otherThing {
 //: ### Challenge 1
 //:
 //: Now see if you can make an `if`/`else` statement that prints out a description of how fast we're moving. I.e. "We're standing still" or "We're moving 88.0 km/h".
-kph
+if kph == 0 {
+    print("We're standing still")
+} else {
+    print("We're moving \(kph) km/h")
+}
 
 
 
@@ -50,6 +54,13 @@ kph
 //: It turns out in this Playground the speed limit is 30km/h. So add a case where if the `kph` variable contains a value greater than 30.0 you should print out "Whoa slow down buddy, 35.9 km/h is too fast for this school zone!".
 //: You'll need to use an `if`, an `else if` and an `else` for this, because we have to handle three cases (not moving, moving under speed limit, and moving over speed limit).
 
+if kph == 0 {
+    print("We're standing still")
+} else if kph <= 30 {
+    print("We're moving \(kph) km/h")
+} else {
+    print("Whoa slow down buddy, \(kph) km/h is too fast for this school zone!")
+}
 
 
 
